aabdb6e81e5fd91c5634d0c11be1ef6cb38b95f4
Flaminia <flaminiacastano@gmail.com>
2022-04-08 14:26:14 -0300

    Funcionamiento de API

--- ClimaApp/Home/HomeViewController.swift
+++ ClimaApp/Home/HomeViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Alamofire
+import PromiseKit
 
 class HomeViewController: UIViewController {
 
@@ -18,11 +20,13 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var lblMin: UILabel!
     @IBOutlet weak var lblDía: UILabel!
     
+    @IBOutlet weak var activityLoading: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpView()
-        
+        txtCiudad.text = "Buenos Aires"
+        searchWeatherCity()
     }
 
     func setUpView() {
@@ -32,6 +36,57 @@ class HomeViewController: UIViewController {
     }
 
     @IBAction func buscarButton(_ sender: Any) {
+        searchWeatherCity()
+    }
+
+    
+    func searchWeatherCity() {
+        activityLoading.isHidden = false
+        activityLoading.startAnimating()
+        
+        let cityTitle = validateEmptyFields(txtCiudad.text!)
+        let city = replaceCityName(cityTitle)
+        
+        firstly {
+            when(fulfilled:
+                    APIClima.weatherPrimises(city),
+                    APIClima.descriptionDayPromises(city)
+            )
+        }
+        .done { (weather, descriptionDay) in
+            self.lblTemperatura.text = "\(Int(weather.temp))°"
+            self.lblMax.text = "\(Int(weather.tempMax))°"
+            self.lblMin.text = "\(Int(weather.tempMin))°"
+            self.lblDía.text = descriptionDay.uppercased()
+            self.lblCiudad.text = cityTitle
+        }
+        .catch { error in
+            let alert = UIAlertController(title: "Error", message: "No se pudo encontrar esa ciudad. Intente de nuevo.", preferredStyle: .alert)
+            
+            let accept = UIAlertAction(title: "Aceptar", style: .default)
+            
+            alert.addAction(accept)
+            self.present(alert, animated: true)
+        }.finally{
+            self.activityLoading.stopAnimating()
+            self.activityLoading.isHidden = true
+        }
+    }
+    
+    func replaceCityName(_ city: String) -> String{
+        return city.replacingOccurrences(of: " ", with: "%20")
+    }
+    
+    func validateEmptyFields(_ city: String) -> String{
+        if city.isEmpty{
+            let alert = UIAlertController(title: "Weather", message: "Complete el campo vacío", preferredStyle: .alert)
+            
+            let accept = UIAlertAction(title: "Aceptar", style: .default)
+            
+            alert.addAction(accept)
+            present(alert, animated: true)
+        }
+            return city
     }
 }
 
