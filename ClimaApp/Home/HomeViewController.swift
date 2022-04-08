42f1e4c408d444a4bffd4744264aace9f55fe184
Flaminia <flaminiacastano@gmail.com>
2022-04-08 12:44:34 -0300

    Maquetado pantalla Home

---
+++ ClimaApp/Home/HomeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  ClimaApp
+//
+//  Created by Flaminia Castaño on 08/04/2022.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var fondoView: UIView!
+    @IBOutlet weak var txtCiudad: UITextField!
+    @IBOutlet weak var lblCiudad: UILabel!
+    
+    @IBOutlet weak var lblTemperatura: UILabel!
+    @IBOutlet weak var lblMax: UILabel!
+    @IBOutlet weak var lblMin: UILabel!
+    @IBOutlet weak var lblDía: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        fondoView.layer.cornerRadius = 15
+        fondoView.layer.borderColor = UIColor(named: "#5CE1E6")?.cgColor
+        fondoView.layer.borderWidth = 2
+    }
+
+
+    @IBAction func buscarButton(_ sender: Any) {
+    }
+}
+
